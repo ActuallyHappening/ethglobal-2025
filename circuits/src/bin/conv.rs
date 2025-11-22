74a5173203a717ff07b99f707fbac9ca771a8877
ActuallyHappening <105958073+ActuallyHappening@users.noreply.github.com>
2025-11-22 17:50:28 -0300

    todo: write parsed values

--- circuits/src/bin/conv.rs
+++ circuits/src/bin/conv.rs
@@ -1,6 +1,7 @@
 use std::str::FromStr;
 use ystd::prelude::*;
 
+use alloy_primitives::Address;
 use alloy_primitives::ruint::aliases::U256;
 use clap::Parser;
 
@@ -13,7 +14,10 @@ struct Cli {
 
 #[derive(clap::Subcommand)]
 enum Conv {
+    ///cargo r --bin conv num-to-bytes $"(500 * 10 ** 15)"
     NumToBytes { num: String },
+    /// cargo r --bin conv address-to-bytes 0xA7E34d70B0E77fD5E1364705f727280691fF8B9a
+    AddressToBytes { addr: Address },
 }
 
 #[tokio::main]
@@ -28,6 +32,10 @@ async fn main() -> color_eyre::Result<()> {
             let num = U256::from_str(&num)?;
             let num = num.as_le_slice();
             info!(?num);
+        }
+        Conv::AddressToBytes { addr } => {
+            let addr = addr.0.0;
+            info!(?addr);
         }
     }
 
